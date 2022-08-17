560773864b1f3ce336ae73b4df691e5b853a464f
Matheus Mendes <contact.matheus.mendes@gmail.com>
2022-08-16 22:21:50 -0300

    setup module and navigation

---
+++ feature/pokemonList/src/main/java/com/matheus/mendes/pokedex/pokemonlist/presentation/PokemonListFragment.kt
@@ -0,0 +1,6 @@
+package com.matheus.mendes.pokedex.pokemonlist.presentation
+
+import androidx.fragment.app.Fragment
+import com.matheus.mendes.pokedex.pokemonlist.R
+
+internal class PokemonListFragment : Fragment(R.layout.fragment_pokemon_list)
