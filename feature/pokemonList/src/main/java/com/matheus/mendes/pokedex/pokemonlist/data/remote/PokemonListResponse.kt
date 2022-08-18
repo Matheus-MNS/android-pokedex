29bc038a162ebcf4feb914bb286b65faeb61c79d
Matheus Mendes <contact.matheus.mendes@gmail.com>
2022-08-17 23:17:35 -0300

    implement service and reponse

---
+++ feature/pokemonList/src/main/java/com/matheus/mendes/pokedex/pokemonlist/data/remote/PokemonListResponse.kt
@@ -0,0 +1,23 @@
+package com.matheus.mendes.pokedex.pokemonlist.data.remote
+
+import com.google.gson.annotations.SerializedName
+
+internal data class PokemonListResponse(
+    @SerializedName("count")
+    val count: Int? = null,
+    @SerializedName("next")
+    val next: String? = null,
+    @SerializedName("previous")
+    val previous: String? = null,
+    @SerializedName("results")
+    val list: List<PokemonResponse>?
+)
+
+internal data class PokemonResponse(
+    @SerializedName("name")
+    val name: String? = null,
+    @SerializedName("url")
+    val url: String? = null
+)
+
+
